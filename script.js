5d5b19f7579350222ce0871e16b9d783cc33c8c1
kimjaeeun <geeins55@naver.com>
2023-09-22 17:04:08 +0900

    Create script.js
    script.js 생성

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("taskInput");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+        if (taskText !== "") {
+            const li = document.createElement("li");
+            li.textContent = taskText;
+            taskList.appendChild(li);
+
+            li.addEventListener("click", function () {
+                li.classList.toggle("completed");
+            });
+
+            taskInput.value = "";
+        }
+    });
+
+    taskInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            addTaskButton.click();
+        }
+    });
+});
